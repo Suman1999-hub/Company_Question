360166b4393e2682dfdc0582efb51c8eeebb723c
SUMAN RANA <68540296+Suman1999-hub@users.noreply.github.com>
2022-01-03 01:40:37 +0530

    Add files via upload

---
+++ PrimeFactor.java
@@ -0,0 +1,24 @@
+package Array;
+import java.util.*;
+public class PrimeFactor {
+    static void primeFact(int n){
+        while(n%2==0){
+            System.out.print(2+" ");
+            n=n/2;
+        }
+        for(int i=3;i<=Math.sqrt(n);i+=2){
+            while(n%i==0){
+                System.out.print(i+" ");
+                n=n/i;
+            }
+        }
+        if(n>2){
+            System.out.print(n);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        primeFact(n);
+    }
+}
